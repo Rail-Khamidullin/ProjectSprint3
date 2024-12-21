6377f3e7618e9ef7b59e81c28f959b914cb526ab
Rail <rail.tehs@gmail.com>
2024-12-21 15:36:04 +0400

    Add fail with constants

--- untitled/src/main/java/pages/QuestionPage.java
+++ untitled/src/main/java/pages/QuestionPage.java
@@ -7,15 +7,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static pages.ConstantsClass.IS_VISIABLE_QUESTION;
+import static pages.ConstantsClass.SUB_HEADER;
+
 public class QuestionPage {
 
-    // Константа с URL самоката
-    public static final String URL_SAMOKAT = "https://qa-scooter.praktikum-services.ru/";
     private WebDriver driver;
-    // Локатор заголовка с вопросами (для скрола)
-    private By subHeader = By.xpath(".//div[text() = 'Вопросы о важном']");
-    // Локатор с блоком вопросов
-    private By isVivsiableQuestion = By.xpath(".//div[@class = 'Home_FAQ__3uVm4']");
 
     public QuestionPage(WebDriver driver) {
         this.driver = driver;
@@ -23,14 +20,14 @@ public class QuestionPage {
 
     // Скрол до подзаголовка с вопросами и ожиданием
     public void scrollQuestion() {
-        WebElement element = driver.findElement(subHeader);
+        WebElement element = driver.findElement(SUB_HEADER);
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", element);
     }
 
     // Ожидание отображения блока с вопросами
     public void waitForQuestionModule() {
         new WebDriverWait(driver, 3)
-                .until(ExpectedConditions.visibilityOfElementLocated(isVivsiableQuestion));
+                .until(ExpectedConditions.visibilityOfElementLocated(IS_VISIABLE_QUESTION));
     }
 
     // Выбор вопроса, ожидание и взятие ответа
